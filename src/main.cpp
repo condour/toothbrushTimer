edf4ce53e42a0372ab8da2a9d4c8b96c0ffee4e6
Michael Condouris <condour@gmail.com>
2022-07-29 18:36:21 -0400

    working with quadrants

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,21 @@
 #define DS_PIN 9
 #define SHIFT_PIN 2
 #define LATCH_PIN 3
-#define CLEAR_PIN 4
-#define RESET_PIN 7
 #define LED_CLOCK 6
 #define LED_DATA 5
+#define QUADRANT_1 A0
+#define QUADRANT_2 A1
+#define QUADRANT_3 A2
+#define QUADRANT_4 A3
+
 #define BEEPER 16
 #define CHANNEL 8
-#define MAX_TOOTH_COUNT 8
+#define MAX_TOOTH_COUNT 128
 #define MAX_ALARM_COUNT 8
 
 #define TIMING_MODE 1
 #define ALARM_MODE 2
 #define OFF_MODE 0
-
-void tone(uint32_t note)
-{
-  
-    ledcAttachPin(BEEPER, CHANNEL);
-  int status = ledcSetup(CHANNEL, 50, 12);
-  Serial.printf("status? %d, note? %d\n", status, note);
-  ledcWriteTone(CHANNEL, note);
-}
-void noTone(void)
-{
-  tone(0);
-}
 
 TM1637Display display(LED_CLOCK, LED_DATA);
 
@@ -47,41 +37,36 @@ const uint8_t SEG_DONE[] = {
 };
 const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
 const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
-struct Button
+const int quadrantPins[] = {QUADRANT_1, QUADRANT_2, QUADRANT_3, QUADRANT_4};
+/* struct Button
 {
   const uint8_t PIN;
   uint32_t numberKeyPresses;
   bool pressed;
 };
+ */
 
-Button resetButton = {RESET_PIN, 0, false};
-void buckaroo()
+void arpeggio(unsigned int *notes, int tempo, int lengthOfSong)
 {
-  tone(NOTE_C7);
-  vTaskDelay(100);
-  tone(NOTE_B6);
-  vTaskDelay(100);
-  tone(NOTE_G6);
-  vTaskDelay(100);
-  noTone();
-}
 
-void arpeggio(int *notes, int tempo)
-{
-  int lengthOfSong = sizeof(notes);
   int i = 0;
-  while (i < lengthOfSong)
+  while (i <= lengthOfSong)
   {
-    tone(notes[i++]);
-    vTaskDelay(tempo);
+    tone(BEEPER, notes[i++], tempo);
+    delay(tempo);
   }
-  noTone();
+  noTone(BEEPER);
   return;
 }
+
+void buckaroo()
+{
+  unsigned int arr[] = {NOTE_C7, NOTE_B6, NOTE_G6, '\0'};
+  arpeggio(arr, 75, 3);
+}
+
 void changeMode(int mode)
 {
-  digitalWrite(CLEAR_PIN, LOW);
-  digitalWrite(CLEAR_PIN, HIGH);
   noTone(BEEPER);
   alarmCount = toothCount = 0;
   if (mode == ALARM_MODE)
@@ -91,13 +76,18 @@ void changeMode(int mode)
   if (mode == OFF_MODE)
   {
     display.setSegments(blank);
+    for (int i = 0; i < 4; i++)
+    {
+
+      digitalWrite(quadrantPins[i], LOW);
+    }
   }
   currentMode = mode;
 }
-
-void IRAM_ATTR isr()
-{
+/*
+ void isr(void) {
   resetButton.numberKeyPresses++;
+  Serial.println("IN RESET?");
   if (currentMode == OFF_MODE)
   {
     changeMode(TIMING_MODE);
@@ -107,54 +97,54 @@ void IRAM_ATTR isr()
     changeMode(OFF_MODE);
   }
 }
-
-void toothTimer(void *pvParameters)
+ */
+void toothTimer()
 {
-  for (;;)
+
+  if (currentMode == TIMING_MODE)
   {
-    if (resetButton.numberKeyPresses)
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DS_PIN, SHIFT_PIN, MSBFIRST, toothCount);
+    digitalWrite(LATCH_PIN, HIGH);
+    if (toothCount == 0 || toothCount == 32 || toothCount == 64 || toothCount == 96)
     {
-      Serial.printf("number key presses %d\n", resetButton.numberKeyPresses);
-      resetButton.numberKeyPresses = 0;
-    }
-    if (currentMode == TIMING_MODE)
-    {
-      digitalWrite(LATCH_PIN, LOW);
-      shiftOut(DS_PIN, SHIFT_PIN, MSBFIRST, toothCount);
-      digitalWrite(LATCH_PIN, HIGH);
-      if (toothCount == 0 || toothCount == 32 || toothCount == 64 || toothCount == 96)
-      {
       //  int arr[] = { NOTE_D6, NOTE_F6, NOTE_G6, NOTE_B6, '\0' };
       //  arpeggio(arr, 200);
+      buckaroo();
+      digitalWrite(quadrantPins[toothCount / 32], HIGH);
+      {
+        delay(150);
         buckaroo();
       }
-      display.showNumberDec(MAX_TOOTH_COUNT - toothCount);
-      toothCount++;
-      if (toothCount == MAX_TOOTH_COUNT)
-      {
-        changeMode(ALARM_MODE);
-      }
+    }
+    display.showNumberDec(MAX_TOOTH_COUNT - toothCount);
+    if (toothCount == MAX_TOOTH_COUNT)
+    {
+      changeMode(ALARM_MODE);
+    }
+    toothCount++;
 
-      vTaskDelay(1000);
-    }
-    if (currentMode == ALARM_MODE)
+    delay(1000);
+  }
+  if (currentMode == ALARM_MODE)
+  {
+    unsigned int arr2[] = {NOTE_C6, NOTE_E6, NOTE_G6, NOTE_C7, NOTE_G6, NOTE_E5, NOTE_C6, NOTE_C5, NOTE_C6, '\0'};
+    arpeggio(arr2, 100, 9);
+
+    delay(sizeof(arr2) * 100);
+    alarmCount++;
+    if (alarmCount == MAX_ALARM_COUNT)
     {
-      int arr[] = {1047, 1319, 1568, '\0'};
-      arpeggio(arr, 100);
-      vTaskDelay(60);
-      alarmCount++;
-      if (alarmCount == MAX_ALARM_COUNT)
-      {
-        changeMode(OFF_MODE);
-      }
+      changeMode(OFF_MODE);
     }
-    if (currentMode == OFF_MODE)
-    {
-      digitalWrite(BEEPER, LOW);
-      digitalWrite(LATCH_PIN, LOW);
-      shiftOut(DS_PIN, SHIFT_PIN, MSBFIRST, 0);
-      digitalWrite(LATCH_PIN, HIGH);
-    }
+  }
+  if (currentMode == OFF_MODE)
+  {
+    digitalWrite(BEEPER, LOW);
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DS_PIN, SHIFT_PIN, MSBFIRST, 0);
+    digitalWrite(LATCH_PIN, HIGH);
+    delay(1000);
   }
 }
 
@@ -170,24 +160,18 @@ void setup()
   pinMode(DS_PIN, OUTPUT);
   pinMode(SHIFT_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
-  pinMode(CLEAR_PIN, OUTPUT);
-  pinMode(RESET_PIN, INPUT_PULLUP);
-  attachInterrupt(RESET_PIN, isr, FALLING);
+  pinMode(QUADRANT_1, OUTPUT);
+  pinMode(QUADRANT_2, OUTPUT);
+  pinMode(QUADRANT_3, OUTPUT);
+  pinMode(QUADRANT_4, OUTPUT);
+  //  attachInterrupt(digitalPinToInterrupt(RESET_PIN), isr, FALLING);
   digitalWrite(BEEPER, LOW);
-  digitalWrite(CLEAR_PIN, LOW);
-  digitalWrite(CLEAR_PIN, HIGH);
-
-  xTaskCreatePinnedToCore(
-      toothTimer,
-      "toothTimer",
-      10000,
-      NULL,
-      1,
-      NULL,
-      ARDUINO_RUNNING_CORE);
+  //  digitalWrite(CLEAR_PIN, LOW);
+  //  digitalWrite(CLEAR_PIN, HIGH);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  toothTimer();
 }
